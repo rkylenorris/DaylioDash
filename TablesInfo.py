2ab1f899d54f7a14f3c9751d0e4c39fb59a2330a
Kyle Norris <r.kyle.norris@gmail.com>
2024-08-28 18:08:08 -0400

    added get methods to classes

--- TablesInfo.py
+++ TablesInfo.py
@@ -1,22 +1,35 @@
 import json
 from typing import List, Dict
 
+
 class Column:
     def __init__(self, column: str, type_name: str, kind: str):
         self.column = column
         self.type_name = type_name
         self.kind = kind
 
+
 class DaylioTable:
-    def __init__(self, columns: List[Column]):
+    def __init__(self, columns: List[Column], name: str):
         self.columns = columns
+        self.name = name
+
+    def get_columns_by_kind(self, kind: str) -> List[Column]:
+        return [x for x in self.columns if x.kind == kind]
+
+    def get_columns_by_name(self, name: str) -> List[Column]:
+        return [x for x in self.columns if x.column == name]
 
     @classmethod
-    def from_json(cls, json_data: str) -> 'DaylioTable':
+    def from_json(cls, json_data: str, table_name: str) -> 'DaylioTable':
         data = json.loads(json_data)
         columns = [Column(**item) for item in data]
-        return cls(columns)
+        return cls(columns, table_name)
+
 
 class DaylioTables:
     def __init__(self, daylio_tables: List[DaylioTable]):
-        self.daylio_tables = daylio_tables
+        self.tables = daylio_tables
+
+    def get_table(self, name: str) -> list[DaylioTable]:
+        return [x for x in self.tables if x.name == name]
--- main.py
+++ main.py
@@ -1,16 +1,60 @@
-# This is a sample Python script.
+import pandas as pd
+import json
+from pathlib import Path
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+data_dir = Path('data')
+table_info_path = Path('table_info')
+
+json_path = Path(data_dir / 'backup_2024_08_22.json')
+
+with open(json_path, 'r') as f:
+    data = json.load(f)
+
+tables_needed = [
+    'customMoods',
+    'tags',
+    'dayEntries',
+    'tag_groups',
+    'goalEntries'
+]
+dfs = []
+for table in tables_needed:
+    df = pd.read_excel(data_dir / f'table_info.xlsx', sheet_name=table)
+    df.to_json(table_info_path / f'table_info_{table}.json', orient='records')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+# for table in tables_needed:
+#     print(table)
+#     df = pd.DataFrame(data[table])
+#     df.to_csv(data_dir / f'{table}.csv', index=False)
+#     with open(data_dir / f'{table}_columns.txt', 'a') as f:
+#         for column in df.columns:
+#             f.write(column + '\n')
+#     print("""
+# -----------------------------------------------------------------------
+#     """)
+#
+#
+# combined_str = ""
+# for file in data_dir.glob('*_columns.txt'):
+#     table_name = file.stem.replace('_columns', '')
+#     with open(file, 'r') as f:
+#         table_columns = f.read()
+#     table_info = f"{table_name}\n-------\n{table_columns}\n\n********************\n\n"
+#     combined_str += table_info
+#
+# with open(data_dir / 'table_info.txt', 'w') as f:
+#     f.write(combined_str)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+# custom_moods_df = pd.DataFrame(data['customMoods'])
+#
+# custom_moods_df['createdAt'] = pd.to_datetime(custom_moods_df['createdAt'], unit='ms')
+#
+# # Display the DataFrame
+# print(custom_moods_df.head())
